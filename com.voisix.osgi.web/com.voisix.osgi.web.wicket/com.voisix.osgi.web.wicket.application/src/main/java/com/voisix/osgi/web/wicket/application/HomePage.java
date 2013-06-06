544432e66143c3d8d1e1bbc5bab35eab09f83f51
assen <assen.sharlandjiev@gmail.com>
2013-06-06 14:16:44 +0200

    implemented pax-wicket-sample with pax-wicket 2.1.0 :( 3.0.0-SNAPSHOT fails to inject spring/blueprint beans.

--- com.voisix.osgi.web/com.voisix.osgi.web.wicket/com.voisix.osgi.web.wicket.application/src/main/java/com/voisix/osgi/web/wicket/application/HomePage.java
+++ com.voisix.osgi.web/com.voisix.osgi.web.wicket/com.voisix.osgi.web.wicket.application/src/main/java/com/voisix/osgi/web/wicket/application/HomePage.java
@@ -1,46 +1,50 @@
 package com.voisix.osgi.web.wicket.application;
 
 
+
 import java.io.Serializable;
-import java.text.MessageFormat;
 
-import javax.inject.Inject;
-
+import org.apache.wicket.ajax.AbstractAjaxTimerBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.protocol.ws.api.WebSocketBehavior;
-import org.apache.wicket.protocol.ws.api.message.ConnectedMessage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-//import org.ops4j.pax.wicket.api.PaxWicketBean;
+import org.apache.wicket.util.time.Duration;
+import org.ops4j.pax.wicket.api.PaxWicketBean;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 	
-	//@PaxWicketBean(name="label.randomInteger")
-	@Inject
-	private IModel<Serializable> labelRandomInteger;
+	@PaxWicketBean(injectionSource=PaxWicketBean.INJECTION_SOURCE_SPRING, name="randomIntegerModel")
+	private IModel<Serializable> randomIntegerModel;
 	
-	private String applicationName;
-	private String sessionId;
-	private Integer pageId;
-
-	private static final Logger log = LoggerFactory.getLogger(HomePage.class);
+	@PaxWicketBean(injectionSource=PaxWicketBean.INJECTION_SOURCE_SPRING, name="sumIntegerModel")
+	private IModel<Serializable> sumIntegerModel;
 	
-	public HomePage() {				
-		final Label label = new Label("label.randomInteger", labelRandomInteger);
-		add(label);
-		add(new WebSocketBehavior() {			
+	@PaxWicketBean(injectionSource=PaxWicketBean.INJECTION_SOURCE_SPRING, name="avgIntegerModel")
+	private IModel<Serializable> avgIntegerModel;
+	
+	public HomePage() {
+		final Label randomIntegerLabel 	= new Label("randomIntegerLabel", 	randomIntegerModel);
+		final Label sumIntegerLabel 	= new Label("sumIntegerLabel", 		sumIntegerModel);
+		final Label avgIntegerLabel 	= new Label("avgIntegerLabel", 		avgIntegerModel);
+		
+		randomIntegerLabel.setOutputMarkupId(true);
+		sumIntegerLabel.setOutputMarkupId(true);
+		avgIntegerLabel.setOutputMarkupId(true);
+				
+		add(randomIntegerLabel);
+		add(sumIntegerLabel);
+		add(avgIntegerLabel);
+		
+		add(new AbstractAjaxTimerBehavior(Duration.seconds(1)) {
 			private static final long serialVersionUID = 1L;
 
 			@Override
-			protected void onConnect(ConnectedMessage message) {
-				applicationName = message.getApplication().getName();
-				sessionId = message.getSessionId();
-				pageId = message.getPageId();
-				
-				log.info(MessageFormat.format("[WebSocket:onConnect][applicationName: {0}][sessionId: {1}][pageId: {2}]", applicationName, sessionId, pageId));
+			protected void onTimer(AjaxRequestTarget target) {
+				target.add(randomIntegerLabel);
+				target.add(sumIntegerLabel);
+				target.add(avgIntegerLabel);
 			}
 		});
 	}
